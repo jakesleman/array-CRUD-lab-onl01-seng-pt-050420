956354b621f03a4b335b80176004cbca4a334dab
Jake Sleman <jakesleman@icloud.com>
2020-05-15 00:50:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  famous_cats = ["lil bub", "grumpy cat", "maru", "nala"]
+  famous_cats.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
