932035bcd71191dd367450258c468819e6ecfebb
Jake Sleman <jakesleman@icloud.com>
2020-05-15 01:17:33 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,3 +1,4 @@
+require "pry"
 def create_an_empty_array
   [ ]
 end
@@ -7,32 +8,38 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  famous_cats = ["lil bub", "grumpy cat", "maru", "nala"]
   
-  famous_cats << "jake cat"
+  array << element
   
 end
 
 def add_element_to_start_of_array(array, element)
   
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
   
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
   
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
   
+ array[index_number]
+ 
 end
 
 def retrieve_first_element_from_array(array)
   
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
   
+  array[-1]
 end
